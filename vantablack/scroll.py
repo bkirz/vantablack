17081fa13d8b13ee0e25816e5e62c5e68f3495b1
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-07-23 15:20:10 -0700

    Move copy logic into random branch as an optimization

--- vantablack/scroll.py
+++ vantablack/scroll.py
@@ -11,27 +11,28 @@ def mmod_bpm(
     """
     Given a simfile, replicates the calculation done by stepmania to determine the
     maximum BPM to use when calculating the scroll modifier for an M-Mod.
-    If a DISPLAYBPM is set (and isn't set to '*'), it uses its max value.
-    Otherwise it finds the max BPM from the file's BPMS field.
+    Uses the maximum DISPLAYBPM if available, otherwise falling back to the max BPM.
 
     If both an SSCSimfile and an SSCChart are provided, and if the chart contains any
     timing fields, the chart will be used as the source of timing.
     """
 
-    # We can safely delegate to displaybpm most of the time, since its max value
-    # is almost always what we want. However, if the displaybpm is set to random ('*'
-    # in the simfile) then we can't get the mmod value that way. This kludge short-circuits
-    # that behavior by forcibly removing '*' values.
-    copied_file = copy.copy(sf)
-    if copied_file.displaybpm == '*':
-        del copied_file.displaybpm
-
-    copied_ssc = copy.copy(ssc_chart)
-    if copied_ssc.displaybpm == '*':
-        del copied_ssc.displaybpm
-
-    match displaybpm.displaybpm(copied_file, copied_ssc):
+    match displaybpm.displaybpm(sf, ssc_chart):
         case displaybpm.StaticDisplayBPM(value=value):
             return value
         case displaybpm.RangeDisplayBPM(max=max):
             return max
+        case displaybpm.RandomDisplayBPM():
+            # We can safely delegate to displaybpm most of the time, since its max value
+            # is almost always the same as the mmod bpm. However, if the displaybpm is set
+            # to random ('*' in the simfile) then we can't get the mmod value that way.
+            # This kludge short-circuits that behavior by forcibly removing '*' values.
+            copied_simfile = copy.copy(sf)
+            if copied_simfile.displaybpm == "*":
+                del copied_simfile.displaybpm
+
+            copied_ssc = copy.copy(ssc_chart)
+            if copied_ssc.displaybpm == "*":
+                del copied_ssc.displaybpm
+
+            return mmod_bpm(copied_simfile, copied_ssc)
