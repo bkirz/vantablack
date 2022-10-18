42b13234bcf22f6a65c86bf039acd318f23ba701
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-10-17 17:30:09 -0700

    Implement ogg_only rule

---
+++ vantablack/rules/ogg_only.py
@@ -0,0 +1,23 @@
+from simfile.dir import SimfileDirectory
+from rule import SongRule, Scope
+
+from vantablack.rule import RuleViolation
+
+class OggOnly(SongRule):
+    name = 'ogg_only'
+    scope = Scope.SONG
+
+    def __init__(self, _config):
+        pass
+
+    def apply(self, song_dir: SimfileDirectory) -> list[RuleViolation]:
+        music_path = song_dir.assets().music
+
+        if not music_path.endswith('.ogg'):
+            return [RuleViolation(
+                message=f"Found non-ogg music file: {music_path}",
+                originating_rule=self,
+                target=song_dir,
+            )]
+        else:
+            return []
