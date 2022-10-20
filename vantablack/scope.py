c6cc72724cf17b04bdbdcf9ecf002e6209ea95db
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-10-19 19:38:55 -0700

    delete some dead code

--- vantablack/scope.py
+++ vantablack/scope.py
@@ -3,23 +3,7 @@ from enum import Enum, unique
 
 @unique
 class Scope(Enum):
-    _ignore_ = "_SCOPE_HIERARCHY"
-
     PACK = "pack"
     SONG = "song"
-    # FILE = 'file'
-    # CHART = 'chart'
-
-    _SCOPE_HIERARCHY: dict["Scope", list["Scope"]] = {
-        PACK: [SONG],
-    }
-
-    def __gt__(self, other: "Scope") -> bool:
-        if other == self:
-            return False
-
-        descendants = self._SCOPE_HIERARCHY.get(self, [])
-        if other in descendants:
-            return True
-
-        return any(descendant > other for descendant in descendants)
+    FILE = "file"
+    CHART = "chart"
